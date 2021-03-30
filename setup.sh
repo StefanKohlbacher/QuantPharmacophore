09a09f07d08b63bf7a9dedf8469d2f7606b7481b
stefan.kohlbacher <stefan.kohlbacher@fiskaly.com>
2021-03-30 14:57:27 +0200

    add attach command in setup script

--- setup.sh
+++ setup.sh
@@ -9,3 +9,4 @@ docker build -t "$IMAGE_NAME" .
 docker rm "$CONTAINER_NAME"
 docker run -dit -P --name "$CONTAINER_NAME" -v ~/container_data:/qphar/data "$IMAGE_NAME"
 docker exec "$CONTAINER_NAME" sh /qphar/prepare_docker_environment.sh
+docker attach "$CONTAINER_NAME"
